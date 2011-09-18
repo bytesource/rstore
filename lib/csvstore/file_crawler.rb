7871088fddb70e261559b747fec14d3c64c8e47c
Stefan Rohlfing <stefan.rohlfing@gmail.com>
2011-09-18 16:56:26 +0800

    started work on FileCrawler class

--- lib/csvstore/file_crawler.rb
+++ lib/csvstore/file_crawler.rb
@@ -1,5 +1,7 @@
 # encoding: utf-8
 
+require 'csvstore/configuration'
+
 module CSVStore
   class FileCrawler
 
@@ -9,13 +11,45 @@ module CSVStore
 
     @file_queue = Hash.new {|h,k| h[k] = Hash.new {|h,k| h[k] = nil}}
 
+    attr_reader :file_options, :parse_options 
+    attr_reader :path_to_file_or_folder
+    attr_reader :file_paths
+    attr_reader :config
+    
+    
+
+    def initialize file_or_folder, options
+      @path_to_file_or_folder = file_or_folder
+      @config = Configuration.new(file_or_folder, options)
+      @file_options  = config.file_options
+      @parse_options = config.parse_options
+    end
+
+    def parse
+      dest = File.expand_path(path)
+      if File.directory?(dest)        # Test if destination is a directory.
+        Dir.chdir(dest)                # Change current directory to 'path'.
+        files = ''
+        if @file_options[:recursive]
+          files = Dir.glob("**/*")       # Recursively read files into array.
+        else
+          # non-recursive
+        end
+        files.each do |f|
+          @file_paths << f
+        end
+      else # file or web address
+        @file_paths << f
+      end
+    end
 
 
-         
-
-
-
-
+    def add
+      @file_paths.each do |path|
+        FileCrawler[path][:file_options]  = @file_options
+        FileCrawler[path][:parse_options] = @parse_options
+      end
+    end
 
   end
 end
--- spec/csvstore/file_crawler_spec.rb
+++ spec/csvstore/file_crawler_spec.rb
@@ -1,4 +1,30 @@
 # encoding: utf-8
 
 require 'spec_helper'
+
+describe CSVStore::FileCrawler do
+  
+  options     = {col_sep: ";", quote_char: "'", recursive: true}
+  test_dir   = '/test_dir/dir_1/dir_2'
+
+  # pp File.directory?(test_dir)
+  # pp File.directory?(File.expand_path(test_dir))
+
+  let(:crawler) { described_class.new(test_dir, options) }
+
+  context "On Initialization" do
+
+    it "should set all variables correctly" do
+      config = CSVStore::Configuration.new(test_dir, options)
+      puts "options: #{options}"
+
+      crawler.path_to_file_or_folder.should == test_dir
+      crawler.file_options.should  == config.file_options
+      crawler.parse_options.should == config.parse_options
+    end
+  end
+
+
+
+end
  
