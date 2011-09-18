1195430a8262862d210c18f1dad4a8e7e1e28480
Stefan Rohlfing <stefan.rohlfing@gmail.com>
2011-09-18 16:54:09 +0800

    added valuation to Configuration class, finished tests

--- lib/csvstore/configuration.rb
+++ lib/csvstore/configuration.rb
@@ -1,5 +1,4 @@
 # encoding: utf-8
-$:.unshift File.expand_path('../../../lib', __FILE__)
 require 'csvstore/core_ext/object'
 
 module CSVStore
@@ -8,35 +7,40 @@ module CSVStore
     # Todo: Evaluate the correctness of values passes via file_options
 
     # Supported options
-    Parse_options   = [:row_sep, :col_sep, :quote_char]
-    File_options    = [:recursive, :has_headers, :selector]
+    KnownParseOptions   = [:row_sep, :col_sep, :quote_char, :field_size_limit, :skip_blanks].freeze
+    KnownFileOptions    = [:recursive, :has_headers, :selector]
 
-    Validation      = {recursive:   lambda { |value| value.boolean_or_nil? },
-                       has_headers: lambda { |value| value.boolean_or_nil? },
-                       selector:    lambda { |value| value.is_a?(Array)    }}
+    Validations         = Hash.new { |h,k| lambda { |value| true }}.
+      merge!({recursive:   lambda { |value| value.boolean_or_nil? },
+              has_headers: lambda { |value| value.boolean_or_nil? },
+              selector:    lambda { |value| value.is_a?(String) }})
 
     attr_reader :file_options, :parse_options
+    attr_reader :path
+    
 
 
 
     def initialize path, all_options
+      all_options = all_options.dup
       self.parse_options = all_options
       self.file_options  = all_options
+      @path = path
 
-      raise ArgumentError, arg_error_message(path, all_options) if all_options.size > 0
+      raise ArgumentError, arg_error_message(@path, all_options) if all_options.size > 0
     end
 
     
     def parse_options= all_options
-      new_settings = extract_options all_options, Parse_options
+      new_settings = extract_options all_options, KnownParseOptions
       @parse_options = new_settings
     end
 
     # GO ON HERE AND CHECK THE INPUT VALUE!!!
 
+   
     def file_options= all_options
-      new_settings = extract_options all_options, File_options
-      # new_settings.each_value do |value|
+      new_settings = extract_options all_options, KnownFileOptions
       @file_options = new_settings
     end
 
@@ -48,84 +52,30 @@ module CSVStore
 
       provided_options_copy = provided_options.dup
 
-      provided_options_copy.inject({}) do |extracted, (key, value)|
-        if supported_options.include?(key)
-        extracted[key] = value 
-        provided_options.delete(key)
+      provided_options_copy.inject({}) do |extracted, (option, value)|
+        if supported_options.include?(option)
+          if valid_value?(option, value)
+            extracted[option] = value 
+            provided_options.delete(option)
+          else
+            raise ArgumentError, "path #{@path}: '#{value}' (#{value.class}) is not a valid value for option '#{option.to_s}'"
+          end
         end
       extracted
       end
     end
 
 
+    def valid_value? option, value
+      Validations[option][value]
+    end
+
+
     def arg_error_message path, all_options
       keys = all_options.keys.join(', ')
-      "Unsupported options: #{keys} on path '#{path}'"
+      "Unsupported options: #{keys} for path '#{path}'"
     end
  
   end
 end
 
-
-
-require 'csv'
-require 'pp'
-
-# Make constant?
-Parse_options   = [:row_sep, :col_sep, :col_char]
-File_options    = [:recursive, :has_headers, :selector]
-
-options_wrong_keys = {col_sep: ";", quote_char: "'", recursive: true, converters: [1,2,3], wrong: 'xxx'}
-options_correct = {col_sep: ";", quote_char: "'", recursive: true}
-
-config = CSVStore::Configuration.new('/home/sovonex/Desktop', options_correct)
-puts "parse options:"
-pp config.parse_options
-puts "file options:"
-pp config.file_options
-
-
-# values at
-
-
-csv = <<-CSV.gsub(/^ +/, "")
-  col1,col2,生日,col4,col5,col6,col7
-  string1,,string3,string4,string5,string6,string7
-CSV
-
-content = CSV.parse(csv, :headers => true)
-# puts ":headers => true"
-# pp content
-# #<CSV::Table mode:col_or_row row_count:2>
-
-
-content = CSV.parse(csv, :headers => false)
-# puts ":headers => false"
-# pp content
-# [["col1", "col2", "生日", "col4", "col5", "col6", "col7"],
-#  ["string1", nil, "string3", "string4", "string5", "string6", "string7"]]
-
-csv_col_sep = <<-CSV.gsub(/^ +/, "")
-  col1;col2;生日;col4;col5;col6;col7
-  string1;;string3;string4;string5;string6;string7
-CSV
-
-content = CSV.parse(csv_col_sep, :col_sep => ';')
-# puts "col_sep: => ';'"
-# pp content
-# [["col1", "col2", "生日", "col4", "col5", "col6", "col7"],
-#  ["string1", nil, "string3", "string4", "string5", "string6", "string7"]]
-
-
-# CONFIGURATION OPTIONS TO CONSIDER:
-
-# CSV
-#
-
-# -----------------------------
-
-# CSVStore
-
-# ------------------------------
-# Reject or silently swallow the following CSV options:
-# 
--- spec/csvstore/configuration_spec.rb
+++ spec/csvstore/configuration_spec.rb
@@ -1,3 +1,57 @@
 # encoding: utf-8
 
 require 'spec_helper'
+
+describe CSVStore::Configuration do
+
+  parse_options = {row_sep: '\n', col_sep: ';', quote_char: "'", field_size_limit: nil, skip_blanks: true}
+  file_options  = {recursive: true, has_headers: true, selector: 'pre div.line'}
+
+  all_options   = parse_options.merge(file_options)
+
+  path = '/home/sovonex/Desktop/temp/test.csv'
+
+  let(:config) { described_class.new(path, all_options) }
+
+  describe "On initialization" do
+
+    context "when successfull" do
+
+      specify { config.parse_options.should == parse_options }
+      specify { config.file_options.should  == file_options }
+      specify { config.path.should  == path }
+    end
+
+    context "on failure" do
+
+      context "when option hash contains unknown option keys" do
+        with_unknown_option = all_options.merge(:unknown => 'some value', :also_unknown => 'some other value')
+
+        it "should throw an exception" do 
+
+          lambda do
+            described_class.new(path, with_unknown_option)
+          end.should raise_exception(ArgumentError, /unknown, also_unknown/)
+        end
+      end
+
+      context "when a file option has the wrong value" do
+        wrong_selector = all_options.merge(:selector => [])            # valid: String
+        wrong_recursive = all_options.merge(:recursive => 'true')      # valid: true, false, nil
+        wrong_has_headers = all_options.merge(:has_headers => 'false') # valid: true, false, nil
+        wrong_values = [wrong_selector, wrong_recursive, wrong_has_headers]
+
+      
+
+        it "should throw an exception" do
+
+          wrong_values.each do |options|
+            lambda do
+              described_class.new(path, options)
+            end.should raise_exception(ArgumentError)
+          end
+        end
+      end
+    end
+  end
+end
