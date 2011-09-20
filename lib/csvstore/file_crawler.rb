1a2dba401fea062ed842ffd5149263c9127e731a
Stefan Rohlfing <stefan.rohlfing@gmail.com>
2011-09-20 15:57:25 +0800

    started testing FileCrawler#parse

--- lib/csvstore/file_crawler.rb
+++ lib/csvstore/file_crawler.rb
@@ -12,42 +12,46 @@ module CSVStore
     @file_queue = Hash.new {|h,k| h[k] = Hash.new {|h,k| h[k] = nil}}
 
     attr_reader :file_options, :parse_options 
-    attr_reader :path_to_file_or_folder
+    attr_reader :path
     attr_reader :file_paths
     attr_reader :config
     
     
 
     def initialize file_or_folder, options
-      @path_to_file_or_folder = file_or_folder
+      @path = file_or_folder
       @config = Configuration.new(file_or_folder, options)
       @file_options  = config.file_options
       @parse_options = config.parse_options
+      @file_paths    = []
     end
 
     def parse
-      dest = File.expand_path(path)
-      if File.directory?(dest)        # Test if destination is a directory.
+      dest = File.expand_path(@path)
+      if File.directory?(dest)
+        puts "Directory"
+        puts "is directory"   # Test if destination is a directory.
         Dir.chdir(dest)                # Change current directory to 'path'.
         files = ''
         if @file_options[:recursive]
           files = Dir.glob("**/*")       # Recursively read files into array.
         else
-          # non-recursive
+          files = Dir.glob("*")          # Read files of the current directory
         end
         files.each do |f|
           @file_paths << f
         end
       else # file or web address
-        @file_paths << f
+        @file_paths << @path
       end
     end
 
 
     def add
+      parse
       @file_paths.each do |path|
-        FileCrawler[path][:file_options]  = @file_options
-        FileCrawler[path][:parse_options] = @parse_options
+        FileCrawler.file_queue[path][:file_options]  = @file_options
+        FileCrawler.file_queue[path][:parse_options] = @parse_options
       end
     end
 
--- spec/csvstore/file_crawler_spec.rb
+++ spec/csvstore/file_crawler_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 describe CSVStore::FileCrawler do
   
   options     = {col_sep: ";", quote_char: "'", recursive: true}
-  test_dir   = '/test_dir/dir_1/dir_2'
+  test_dir   = '../test_dir/dir_1/dir_2'
 
   # pp File.directory?(test_dir)
   # pp File.directory?(File.expand_path(test_dir))
@@ -16,15 +16,25 @@ describe CSVStore::FileCrawler do
 
     it "should set all variables correctly" do
       config = CSVStore::Configuration.new(test_dir, options)
-      puts "options: #{options}"
 
-      crawler.path_to_file_or_folder.should == test_dir
+      crawler.path.should == test_dir
       crawler.file_options.should  == config.file_options
       crawler.parse_options.should == config.parse_options
     end
   end
 
+  describe :file_queue do
 
+    context "with default options for 'recursive'" do
 
+      let(:crawler) { described_class.new(test_dir, options)}
+
+      it "should add return a hash with of the kind 'individual_filename => options'" do
+
+        crawler.add
+        CSVStore::FileCrawler.file_queue.should == {'empty.csv' => {:file_options=>{:recursive=>true, :has_headers=>true}, 
+                                                                    :parse_options=>{:col_sep=>";", :quote_char=>"'"}}}
+      end
+    end
+  end
 end
- 
