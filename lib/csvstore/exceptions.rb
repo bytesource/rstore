f500d8662419504f9f12e2358c393acf4d1d52a3
Stefan Rohlfing <stefan.rohlfing@gmail.com>
2011-09-27 14:50:08 +0800

    removed old files from git after renaming project

--- lib/csvstore/exceptions.rb
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-
-module RStore
-  # The error thrown when the length of a row does not fit the number of columns in the db table.
-  class InvalidRowLengthError < StandardError; end
-  class NullNotAllowedError   < StandardError; end
-
-end
-
--- lib/csvstore/version.rb
+++
@@ -1,3 +0,0 @@
-module CSVTable
-  VERSION = "0.0.1"
-end
