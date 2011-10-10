35d4b20363282201f9b727b74f67d66631dd65fc
Stefan Rohlfing <stefan.rohlfing@gmail.com>
2011-10-10 11:39:38 +0800

    implemented CSV#create_table, CSV#read_data, CSV#run

--- lib/rstore/csv.rb
+++ lib/rstore/csv.rb
@@ -7,6 +7,7 @@ require 'rstore/file_crawler'
 require 'rstore/converter'
 require 'rstore/base_db'
 require 'rstore/base_table'
+require 'rstore/core_ext/string'
 
 # RStore::CSV.new do
 #   from '~/temp/', header: true, recursive: true
@@ -28,7 +29,10 @@ module RStore
 
     def initialize &block
       @files_with_options = {}
-      @data_strem = []
+      @data_stream        = []
+      @database           = nil
+      @table              = nil
+      @run                = false
 
       instance_eval(block) if block_given?
 
@@ -47,7 +51,7 @@ module RStore
       database, table = db_table.split('.')
 
       database = BaseDB.db_classes[database.to_sym]
-      table    = BaseTable.table_classes[table_db]
+      table    = BaseTable.table_classes[table.to_sym]
 
       raise Exception, "Database '#{database}' not found"  if database.nil?
       raise Exception, "Table '#{table}' not found"        if table.nil?
@@ -61,49 +65,67 @@ module RStore
     # Sequel.connect('sqlite://blog.db'){|db| puts db[:users].count}
 
     def run
+      raise Exception, "You can invoke the 'run' method only once on a single instance of RStore::CSV"  if @run == true
       raise Exception, "Please specify at least one source file using the 'from' method" if @files_and_options.empty?
+      raise Exception, "Please specify a valid database and table name using the 'to' method" if @database.nil? || @table.nil?
 
       @files_with_options.each do |path, options|
-        csv = CSV.read(path, options[:parse_options])
-        data = Data.new(path, csv, :parsed)
-        @data_stream << data
+        data = read_data path, options[:file_options] 
+        @data_stream << Data.new(path, data, :raw, options)  
       end
 
       Sequel.connect(@database.connection_info) do |db|
 
-        table_name = @table.name
+        create_table
+        name = @table.name
 
-        schema = db.schema(table_name)
-
-        # Create table (consider creating the table inside Storage#insert)
-        # Implement  Data#to_csv, Data#convert, Data#insert
-        # add known state = :raw
-
-        @data_stream.each do |data|
-          converter = Converter.new(data, schema)
-          data = converter.conver
-          storage = Storage.new(data, db, table_name)
-          storage.insert
+        @datastream.each do |data_object|
+          data_object.to_csv.convert(@database, name).insert(@database, name)
         end
+
+        @run = true
       end
     end
 
 
 
+    def read_data path, options
+      data = ''
+      if path.url?
+
+        doc = Nokogiri::HTML(open(path))
+
+        selector = options[:selector]
+        data = doc.css(selector).inject("") do |result, link|
+          result << link.content << "\n"
+          result
+        end
+      else
+        data = File.read(path)
+      end
+      data
+    end
 
 
+    def create_table
 
+      name = @table.name
 
+      unless @database.table.exists?(name)
+        @database.create_table(name, @table.table_info)
+      end
 
+      # http://stackoverflow.com/questions/1671401/unable-to-output-mysql-tables-which-involve-dates-in-sequel
+      Sequel::MySQL.convert_invalid_date_time = nil  if @database.connection_info[:adapter] = 'mysql'
+    end
     
-    
-
-    # After 'table create':
-    # http://stackoverflow.com/questions/1671401/unable-to-output-mysql-tables-which-involve-dates-in-sequel
-    # Sequel::MySQL.convert_invalid_date_time = nil  if TempDB.connection_info[:adapter] = 'mysql'
     
     def delimiter_correct? name
       !!(name =~ /^[^\.]+\.[^\.]+$/)
+    end
+
+    def run?
+      @run == true
     end
 
   end
