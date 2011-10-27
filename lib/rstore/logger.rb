ec00a1d0e644210e98447f132ac8d596528531c8
Stefan Rohlfing <stefan.rohlfing@gmail.com>
2011-10-27 11:20:43 +0800

    removed previously commented code

--- lib/rstore/logger.rb
+++ lib/rstore/logger.rb
@@ -5,26 +5,6 @@ require 'rstore/exceptions'
 module RStore
   class Logger
 
-    # class << self
-    #   attr_accessor :error_queue
-    # end
-
-   
-    # http://aberant.tumblr.com/post/4639094626/ruby-drive-by-autovivification
-    # @error_queue = Hash.new {|h,k| h[k] = Hash.new(&h.default_proc)}
-    # @error_queue = Hash.new {|h,k| h[k] = Hash.new{|h,k| h[k] = []}}
-
-    # def self.log(path, state, error, optional_info={})
-    #   main_info = Hash[:error, error.class, :message, error.to_s]
-    #   main_info.merge!(optional_info)
-    #   @error_queue[path][state] << main_info
-    # end
-
-
-    #def self.empty_error_queue
-    #  @error_queue = Hash.new {|h,k| h[k] = Hash.new{|h,k| h[k] = []}}
-    #end
-
     attr_accessor :data
     attr_accessor :message
     
