040acebd8bf1916947044546a275e240a1cf8126
Stefan Rohlfing <stefan.rohlfing@gmail.com>
2011-12-08 11:11:29 +0800

    added documentation for :digit_seps to #from

--- lib/rstore/csv.rb
+++ lib/rstore/csv.rb
@@ -14,19 +14,19 @@ module RStore
 
     #@return [BaseDB] a subclass of {RStore::BaseDB}
     attr_reader :database
-    #@return [BaseTable] a sublcass of {RStore::BaseTable} 
+    #@return [BaseTable] a sublcass of {RStore::BaseTable}
     attr_reader :table
     #@return [Array<Data>] holds `RStore::Data` objects that are used internally to store information from a data source.
     attr_reader :data_array
-    
 
-    # This constructor takes a block yielding an implicit instance of _self_.   
-    # Within the block, the following methods need to be called: 
+
+    # This constructor takes a block yielding an implicit instance of _self_.
+    # Within the block, the following methods need to be called:
     #
     # * {#from}
     # * {#to}
     # * {#run}
-    # @example  
+    # @example
     #  RStore::CSV.new do
     #    from '../easter/children', :recursive => true                   # select a directory or
     #    from '../christmas/children/toys.csv'                           # file, or
@@ -50,26 +50,31 @@ module RStore
     end
 
 
-    # Specify the source of the csv file(s) 
+    # Specify the source of the csv file(s)
     # There can be several calls to this method on given instance of `RStore::CSV`.
     # This method has to be called before {#run}.
     # @overload from(source, options)
     #  @param [String] source The relative or full path to a directory, file, or an URL
-    #  @param [Hash] options The options used to customize fetching and parsing of csv data  
-    #  @option options [Boolean] :has_headers When set to false, the first line of a file is processed as data, otherwise it is discarded. 
+    #  @param [Hash] options The options used to customize fetching and parsing of csv data
+    #  @option options [Boolean] :has_headers When set to false, the first line of a file is processed as data, otherwise it is discarded.
     #    (default: `true`)
-    #  @option options [Boolean] :recursive When set to true and a directory is given, recursively search for files. Non-csv files are skipped. 
+    #  @option options [Boolean] :recursive When set to true and a directory is given, recursively search for files. Non-csv files are skipped.
     #    (default: `false`]
-    #  @option options [String] :selector Mandatory css selector with an URL. Used the same syntax as Nokogiri, default: `""`
+    #  @option options [String] :selector Mandatory css selector when fetching data from an URL. Uses the same syntax as {http://nokogiri.org/ Nokogiri}, default: `""`
     #  @option options [String] :col_sep The String placed between each field. (default: `","`)
-    #  @option options [String, Symbol] :row_sep The String appended to the end of each row. 
+    #  @option options [String, Symbol] :row_sep The String appended to the end of each row.
     #    (default: `:auto`)
     #  @option options [String] :quote_car The character used to quote fields.
     #    (default: `'"'`)
     #  @option options [Integer, Nil] :field_size_limit The maximum size CSV will read ahead looking for the closing quote for a field.
     #    (default: `nil`)
     #  @option options [Boolean] :skip_blanks When set to a true value, CSV will skip over any rows with no content.
-    #    (default: `false`) 
+    #    (default: `false`)
+    #  @option options [Array] :digit_seps The *thousands separator* and *decimal mark* used for numbers in the data source
+    #    (default: `[',', '.']`).
+    #    Different countries use different thousands separators and decimal marks, and setting this options ensures that
+    #    parsing of these numbers succeeds. Note that all numbers will still be *stored* in the format that Ruby recognizes,
+    #    that is with a point (.) as the decimal mark.
     # @overload from(source)
     #  @param [String] source The relative or full path to a directory, file, or an URL. The default options will be used.
     # @return [void]
@@ -90,7 +95,7 @@ module RStore
 
     # Choose the database table to store the csv data into.
     # This method has to be called before {#run}.
-    # @param [String] db_table The names of the database and table, separated by a dot, e.g. 'database.table'. 
+    # @param [String] db_table The names of the database and table, separated by a dot, e.g. 'database.table'.
     #  The name of the database has to correspond to a subclass of `RStore::BaseDB`:
     #  CompanyDB < RStore::BaseDB -> 'company'
     #  The name of the table has to correspond to a subclass of `RStore::BaseTable`:
@@ -125,13 +130,13 @@ module RStore
     # Both methods, {#from} and {#to}, have to be called before this method.
     # @return [void]
     def run
-      return  if ran_once?   # Ignore subsequent calls to #run 
+      return  if ran_once?   # Ignore subsequent calls to #run
       raise Exception, "At least one method 'from' has to be called before method 'run'"  unless @from == true
       raise Exception, "Method 'to' has to be called before method 'run'"                 unless @to   == true
 
       @data_hash.each do |path, data|
         content = read_data(data)
-        @data_array << Data.new(path, content, :raw, data.options)  
+        @data_array << Data.new(path, content, :raw, data.options)
       end
 
       @database.connect do |db|
@@ -191,7 +196,7 @@ module RStore
           end
         else
           content = File.read(path)
-        end 
+        end
 
       raise ArgumentError, "Empty content!"  if content.empty?
 
@@ -200,7 +205,7 @@ module RStore
         logger.log(:fetch, e)
         logger.error
       end
-      
+
       content
     end
 
@@ -215,7 +220,7 @@ module RStore
           # http://sequel.rubyforge.org/rdoc/files/doc/release_notes/2_10_0_txt.html
           Sequel::MySQL.default_engine = 'InnoDB'
           # http://stackoverflow.com/questions/1671401/unable-to-output-mysql-tables-which-involve-dates-in-sequel
-          Sequel::MySQL.convert_invalid_date_time = nil 
+          Sequel::MySQL.convert_invalid_date_time = nil
         end
       end
 
@@ -231,9 +236,9 @@ module RStore
     # @return [void]
     # @yieldparam [Sequel::Dataset] table The dataset of your table
     # @example
-    #  RStore::CSV.query('company.products') do |table|    # table = Sequel::Dataset object 
-    #    table.all                                         # fetch everything 
-    #    table.all[3]                                      # fetch row number 4 
+    #  RStore::CSV.query('company.products') do |table|    # table = Sequel::Dataset object
+    #    table.all                                         # fetch everything
+    #    table.all[3]                                      # fetch row number 4
     #    table.filter(:id => 2).update(:on_stock => true)  # update entry
     #    table.filter(:id => 3).delete                     # delete entry
     #  end
@@ -244,8 +249,8 @@ module RStore
       end
     end
 
-    
-    
+
+
     #@private
     def self.delimiter_correct? name
       !!(name =~ /^[^\.]+\.[^\.]+$/)
@@ -258,11 +263,11 @@ module RStore
     end
 
 
-    # Change default options recognized by {#from}  
-    # The new option values apply to all following instances of `RStore::CSV`  
-    # Options can be reset to their defaults by calling {.reset_default_options}  
-    # See {#from} for a list of all options and their default values.  
-    # @param [Hash] options Keys from default options with their respective new values.  
+    # Change default options recognized by {#from}
+    # The new option values apply to all following instances of `RStore::CSV`
+    # Options can be reset to their defaults by calling {.reset_default_options}
+    # See {#from} for a list of all options and their default values.
+    # @param [Hash] options Keys from default options with their respective new values.
     # @return [void]
     # @example
     #   # Search directories recursively and handle the first row of a file as data by default
@@ -271,11 +276,11 @@ module RStore
       Configuration.change_default_options(options)
     end
 
-   
+
 
     # Reset the options recognized by {#from} to their default values.
     # @return [void]
-    # @example 
+    # @example
     #   RStore::CSV.reset_default_options
     def self.reset_default_options
       Configuration.reset_default_options
@@ -285,4 +290,4 @@ module RStore
   end
 end
 
- 
+
