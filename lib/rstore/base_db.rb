7a52b2b226ea11c2e5864e89b301c3f6f9113df8
Stefan Rohlfing <stefan.rohlfing@gmail.com>
2011-12-13 12:54:55 +0800

    added comment to identify 'self' more clear

--- lib/rstore/base_db.rb
+++ lib/rstore/base_db.rb
@@ -7,7 +7,7 @@ module RStore
 
     class << self
       # A Hash holding subclasses of {RStore::BaseDB}
-      # @return [Hash{Symbol=>BaseDB}] All subclasses of {RStore::BaseDB} defined in the current namespace.  
+      # @return [Hash{Symbol=>BaseDB}] All subclasses of {RStore::BaseDB} defined in the current namespace.
       #   Subclasses are added automatically via _self.inherited_.
       # @example
       #   class CompanyDB < RStore::BaseDB
@@ -30,17 +30,17 @@ module RStore
       BaseDB.db_classes[subclass.name] = subclass
     end
 
-   
+
     # Define the database connection.
     # @note To be called when defining a _subclass_ of {RStore::BaseDB}
     # Accepts the same _one_ _arity_ parameters as [Sequel.connect](http://sequel.rubyforge.org/rdoc/files/doc/opening_databases_rdoc.html)
     # @overload info(options)
     #  @param [String, Hash] connection_info Either a connection string such as _postgres://user:password@localhost/blog_, or a `Hash` with the following options:
-    #  @option options [String] :adapter The SQL database used, such as _mysql_ or _postgres_ 
+    #  @option options [String] :adapter The SQL database used, such as _mysql_ or _postgres_
     #  @option options [String] :host Example: 'localhost'
-    #  @option options [String] :user 
-    #  @option options [String] :password 
-    #  @option options [String] :database The database name. You don't need to provide this option, as its value will be inferred from the class name. 
+    #  @option options [String] :user
+    #  @option options [String] :password
+    #  @option options [String] :database The database name. You don't need to provide this option, as its value will be inferred from the class name.
     #  @return [void]
     # @example
     #  # Using a connection string
@@ -50,29 +50,33 @@ module RStore
     #
     #  # Using an options hash
     #  class CompanyDB < RStore::BaseDB
-    #    info(adapter: 'mysql', 
-    #         host: 'localhost', 
-    #         user: 'root', 
+    #    info(adapter: 'mysql',
+    #         host: 'localhost',
+    #         user: 'root',
     #         password: 'xxx')
     #  end
     def self.info hash_or_string
-      class << self            # self = #<Class:CompanyDB>
+      # self = CompanyDB
+
+      class << self
+        # self = #<Class:CompanyDB>
         attr_reader :connection_info
       end
-                               # self = CompanyDB
+
+      # Instance variables always belong to self.
       @connection_info = hash_or_string.is_a?(Hash) ? hash_or_string.merge(:database => self.name.to_s): hash_or_string
     end
 
 
-    # Uses the connection info from {.info} to connect to the database.  
+    # Uses the connection info from {.info} to connect to the database.
     # @note To be called when defining a _subclass_ of {RStore::BaseDB}
     # @yieldparam [Sequel::Database] db The opened Sequel {http://sequel.rubyforge.org/rdoc/classes/Sequel/Database.html Database} object, which is closed when the block exits.
     # @return [void]
     # @example
     #  class CompanyDB < RStore::BaseDB
-    #    info(adapter: 'mysql', 
-    #         host: 'localhost', 
-    #         user: 'root', 
+    #    info(adapter: 'mysql',
+    #         host: 'localhost',
+    #         user: 'root',
     #         password: 'xxx')
     #  end
     #
@@ -110,10 +114,10 @@ module RStore
     #
     #  CompanyDB.name
     #  #=> :company
-    def self.name  
+    def self.name
       super.gsub!(/DB/,'').downcase.to_sym
     end
 
   end
 end
- 
+
